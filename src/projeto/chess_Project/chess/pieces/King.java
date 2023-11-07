a1fabc4cc710438b40228610813e2d4ea73334ae
leonardo <leonardo.souza.santosalencar@gmail.com>
2023-11-06 23:07:20 -0300

    Implementando movimentos do rei

--- src/projeto/chess_Project/chess/pieces/King.java
+++ src/projeto/chess_Project/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package projeto.chess_Project.chess.pieces;
 
 import projeto.chess_Project.board.Board;
+import projeto.chess_Project.board.Position;
 import projeto.chess_Project.chess.ChessPiece;
 import projeto.chess_Project.chess.Color;
 
@@ -17,8 +18,45 @@ public class King extends ChessPiece {
 
     @Override
     public boolean[][] possibleMoves() {
-        // TODO Auto-generated method stub
-        return null;
+
+        
+        boolean[][] mat = new boolean[getBoard().getColumns()][getBoard().getColumns()];
+        Position pos = new Position(0, 0);
+                
+        //Acima
+
+        pos.setValues(position.getRow() - 1,position.getColumn()-1);
+        while (getBoard().positionExists(pos) && (pos.getColumn() - position.getColumn() <=  Math.abs(1))) {
+            mat[pos.getRow()][pos.getColumn()] = canMove(pos);
+            pos.setColumn(pos.getColumn() + 1);
+        }
+
+        pos.setValues(position.getRow() + 1,position.getColumn()-1);
+        while (getBoard().positionExists(pos) && (pos.getColumn() - position.getColumn() <=  Math.abs(1))) {
+            mat[pos.getRow()][pos.getColumn()] = canMove(pos);
+            pos.setColumn(pos.getColumn() + 1);
+        }
+
+        pos.setValues(position.getRow(), position.getColumn() - 1);
+        while (getBoard().positionExists(pos) && (pos.getColumn() - position.getColumn() <=  Math.abs(1))) {
+            if(getBoard().getPiece(pos) != this){
+                mat[pos.getRow()][pos.getColumn()] = canMove(pos);
+            }
+            pos.setColumn(pos.getColumn() + 1);
+
+        }
+        return mat;
+    }
+
+    private boolean canMove(Position position){
+        ChessPiece piece =(ChessPiece) this.getBoard().getPiece(position);
+
+        if(piece != null){
+            return this.getColor() != piece.getColor();
+        }
+
+        return true;
+        
     }
 
     
